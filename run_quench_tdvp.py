7af11cc57e48ca6acdbb39ac22577057aaf644e3
hyunyonglee <hyunyong.rhee@gmail.com>
2024-02-13 10:20:30 +0900

    test

--- run_quench_tdvp.py
+++ run_quench_tdvp.py
@@ -265,6 +265,7 @@ if __name__ == "__main__":
 
     product_state1 = MPS.from_product_state(DBHM0.lat.mps_sites(), product_state, bc=DBHM0.lat.bc_MPS)
     product_state2 = MPS.from_product_state(DBHM0.lat.mps_sites(), flip_array(product_state), bc=DBHM0.lat.bc_MPS)
+    print(flip_array(product_state))
 
     # ground state
     dmrg_params['orthogonal_to'] = [product_state1]
